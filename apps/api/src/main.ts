48d78147fdb4ad9cf0191cd23edb528667f097c8
Shashika <shashikaxp@gmail.com>
2021-08-28 12:17:58 +1000

    add first typeorm entity

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,38 @@
 import * as express from 'express';
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+
+import { User } from './entity/User';
 
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api!' });
-});
+const main = async () => {
+  await createConnection({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    database: 'ithiri',
+    synchronize: true,
+    username: 'postgres',
+    password: 'postgres',
+    entities: [User],
+  });
 
-const port = process.env.port || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
-});
-server.on('error', console.error);
+  app.get('/api', async (req, res) => {
+    const user = new User();
+    user.firstName = 'Timber';
+    user.lastName = 'Saw';
+    user.isActive = true;
+    const data = await user.save();
+    console.log(data);
+    res.send({ message: 'Welcome to api!' });
+  });
+
+  const port = process.env.port || 3333;
+  const server = app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}/api`);
+  });
+  server.on('error', console.error);
+};
+
+main().catch((error) => console.log(error));
