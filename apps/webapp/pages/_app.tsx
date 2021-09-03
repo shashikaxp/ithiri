bdf6197ae385e304fd69506725c08bbaa44d9059
Shashika <shashikaxp@gmail.com>
2021-09-03 11:16:49 +1000

    fix

--- apps/webapp/pages/_app.tsx
+++ apps/webapp/pages/_app.tsx
@@ -1,8 +1,24 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
 import 'tailwindcss/tailwind.css';
 
-function App() {
+function App({ Component, pageProps }: AppProps) {
   return (
-    <div></div>
+    <>
+    <Head>
+      <title>Welcome to webapp2!</title>
+    </Head>
+    <div className="app">
+      <header className="flex">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src="/nx-logo-white.svg" alt="Nx logo" width="75" height="50" />
+        <h1>Welcome to webapp2!</h1>
+      </header>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </div>
+  </>
   );
 }
 
