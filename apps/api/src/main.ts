09a882853b16f5cb79eb596b7ab65a22481e52aa
Shashika <shashikaxp@gmail.com>
2021-09-24 18:47:58 +1000

    fix

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -84,7 +84,7 @@ const main = async () => {
       cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
         httpOnly: true,
-        sameSite: 'lax',
+        sameSite: 'none',
         secure: false, // use only in https,
       },
       saveUninitialized: false,
