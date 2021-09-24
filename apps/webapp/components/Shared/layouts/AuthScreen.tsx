b150056585c240a0fde0b22487b425d0073511e0
Shashika <shashikaxp@gmail.com>
2021-09-25 09:47:42 +1000

    handle responsiveness of auth screen layout

--- apps/webapp/components/Shared/layouts/AuthScreen.tsx
+++ apps/webapp/components/Shared/layouts/AuthScreen.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const AuthScreen: React.FC = ({ children }) => {
   return (
     <div className="flex  h-screen">
-      <div className="bg-store w-2/3 h-full bg-cover bg-right"></div>
+      <div className="bg-store md:w-1/2 lg:w-2/3 h-full bg-cover bg-right hidden md:block"></div>
       <div className="max-h-screen overflow-auto flex-grow">{children}</div>
     </div>
   );
