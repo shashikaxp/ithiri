dc8d8a80302a6c8ab27eda2bbff13aea22b38113
Shashika <shashikaxp@gmail.com>
2021-09-25 10:20:10 +1000

    move apollo config to util file

--- apps/webapp/pages/_app.tsx
+++ apps/webapp/pages/_app.tsx
@@ -1,10 +1,11 @@
 import './styles.css';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import { offsetLimitPagination } from '@apollo/client/utilities';
+import { ApolloProvider } from '@apollo/client';
+
 import { CookiesProvider } from 'react-cookie';
 import { NextPage } from 'next';
 import { ReactElement, ReactNode } from 'react';
 import { AppProps } from 'next/dist/shared/lib/router/router';
+import { getApollo } from '../util/configApollo';
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -14,133 +15,13 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
-const formatter = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD',
-});
-
-const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_API_URL,
-  cache: new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          getStoreItems: offsetLimitPagination(),
-        },
-      },
-      StorePriceDetails: {
-        fields: {
-          cwPrice: {
-            read(price) {
-              if (!price) return '-';
-              return formatter.format(price);
-            },
-          },
-          nwPrice: {
-            read(price) {
-              if (!price) return '-';
-              return formatter.format(price);
-            },
-          },
-          cwSavings: {
-            read(saving) {
-              if (!saving) return '-';
-              return formatter.format(saving);
-            },
-          },
-          nwSavings: {
-            read(saving) {
-              if (!saving) return '-';
-              return formatter.format(saving);
-            },
-          },
-          cwDiscount: {
-            read(discount) {
-              if (!discount) return '-';
-              if (discount % 1 === 0.0) {
-                return `${Math.floor(discount).toString()}%`;
-              } else {
-                return `*${Math.floor(discount).toString()}%`;
-              }
-            },
-          },
-          nwDiscount: {
-            read(discount) {
-              if (!discount) return '-';
-              if (discount % 1 === 0.0) {
-                return `${Math.floor(discount).toString()}%`;
-              } else {
-                return `*${Math.floor(discount).toString()}%`;
-              }
-            },
-          },
-        },
-      },
-      ShoppingList: {
-        fields: {
-          totalSavings: {
-            read(price) {
-              return formatter.format(price);
-            },
-          },
-        },
-      },
-      ShoppingItem: {
-        fields: {
-          originalPrice: {
-            read(price) {
-              if (!price) return '-';
-              return formatter.format(price);
-            },
-          },
-          price: {
-            read(price) {
-              if (!price) return '-';
-              return formatter.format(price);
-            },
-          },
-          saving: {
-            read(saving) {
-              if (!saving) return '-';
-              return formatter.format(saving);
-            },
-          },
-          discount: {
-            read(discount) {
-              if (!discount) return '-';
-              if (discount % 1 === 0.0) {
-                return `${Math.floor(discount).toString()}%`;
-              } else {
-                return `*${Math.floor(discount).toString()}%`;
-              }
-            },
-          },
-          total: {
-            read(total) {
-              if (!total) return '-';
-              return formatter.format(total);
-            },
-          },
-          quantity: {
-            read(quantity) {
-              if (!quantity) return '-';
-              return quantity;
-            },
-          },
-        },
-      },
-    },
-  }),
-  credentials: 'include',
-});
-
 function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
     <>
       <CookiesProvider>
-        <ApolloProvider client={client}>
+        <ApolloProvider client={getApollo()}>
           {getLayout(<Component {...pageProps} />)}
         </ApolloProvider>
       </CookiesProvider>
---
+++ apps/webapp/util/configApollo.ts
@@ -0,0 +1,115 @@
+import { InMemoryCache, ApolloClient } from '@apollo/client';
+import { offsetLimitPagination } from '@apollo/client/utilities';
+
+const formatCurrency = (amount: number) => {
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
+  if (!amount) return '-';
+  return formatter.format(amount);
+};
+
+const formatDiscount = (discount: number) => {
+  if (!discount) return '-';
+  if (discount % 1 === 0.0) {
+    return `${Math.floor(discount).toString()}%`;
+  } else {
+    return `*${Math.floor(discount).toString()}%`;
+  }
+};
+
+export const getApollo = () => {
+  return new ApolloClient({
+    uri: process.env.NEXT_PUBLIC_API_URL,
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            getStoreItems: offsetLimitPagination(),
+          },
+        },
+        StorePriceDetails: {
+          fields: {
+            cwPrice: {
+              read(price) {
+                return formatCurrency(price);
+              },
+            },
+            nwPrice: {
+              read(price) {
+                return formatCurrency(price);
+              },
+            },
+            cwSavings: {
+              read(saving) {
+                return formatCurrency(saving);
+              },
+            },
+            nwSavings: {
+              read(saving) {
+                return formatCurrency(saving);
+              },
+            },
+            cwDiscount: {
+              read(discount) {
+                return formatDiscount(discount);
+              },
+            },
+            nwDiscount: {
+              read(discount) {
+                return formatDiscount(discount);
+              },
+            },
+          },
+        },
+        ShoppingList: {
+          fields: {
+            totalSavings: {
+              read(price) {
+                return formatCurrency(price);
+              },
+            },
+          },
+        },
+        ShoppingItem: {
+          fields: {
+            originalPrice: {
+              read(price) {
+                return formatCurrency(price);
+              },
+            },
+            price: {
+              read(price) {
+                return formatCurrency(price);
+              },
+            },
+            saving: {
+              read(saving) {
+                return formatCurrency(saving);
+              },
+            },
+            discount: {
+              read(discount) {
+                return formatDiscount(discount);
+              },
+            },
+            total: {
+              read(total) {
+                return formatCurrency(total);
+              },
+            },
+            quantity: {
+              read(quantity) {
+                if (!quantity) return '-';
+                return quantity;
+              },
+            },
+          },
+        },
+      },
+    }),
+    credentials: 'include',
+  });
+};
