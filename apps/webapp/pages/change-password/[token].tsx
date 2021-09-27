584db3412c9a4ffc56b91a24468b03457d2d211e
Shashika <shashikaxp@gmail.com>
2021-09-27 14:52:59 +1000

    update change password UI with new layout

--- apps/webapp/pages/change-password/[token].tsx
+++ apps/webapp/pages/change-password/[token].tsx
@@ -1,20 +1,32 @@
-import React, { useState } from 'react';
+import React, { ReactElement, ReactNode, useState } from 'react';
 import { NextPage } from 'next';
 import NextLink from 'next/link';
 import { Formik, Form } from 'formik';
 import router from 'next/router';
+import Image from 'next/image';
 
 import { InputField } from '../../components/Shared/forms/InputField';
 import { useChangePasswordMutation } from './../../generated/graphql';
 import { toErrorMap } from './../../../webapp/util/toErrorMap';
+import { AuthScreen } from './../../../webapp/components/Shared/layouts/AuthScreen';
+import { Button } from './../../../webapp/components/Shared/ui/Button';
+import Logo from './../../assets/img/logo.png';
 
-const ChangePassword: NextPage<{ token: string }> = ({ token }) => {
-  const [changePassword] = useChangePasswordMutation();
+const ChangePassword: NextPage<{ token: string }> & {
+  getLayout: (page: ReactElement) => ReactNode;
+} = ({ token }) => {
+  const [changePassword, { loading }] = useChangePasswordMutation();
   const [tokenError, setTokenError] = useState('');
 
   return (
     <div>
       <div className="flex flex-col items-center justify-center h-screen">
+        <div className="w-56 flex justify-center">
+          <Image src={Logo} alt="logo" />
+        </div>
+        <div className="font-bold text-text text-2xl mt-8 mb-8">
+          Reset Password
+        </div>
         <Formik
           initialValues={{
             newPassword: '',
@@ -43,20 +55,20 @@ const ChangePassword: NextPage<{ token: string }> = ({ token }) => {
               name="newPassword"
               type="password"
               placeholder="New Password"
+              required
             />
-            <button
-              className="bg-blue-700 rounded-lg text-gray-300 px-4 py-2"
-              type="submit"
-            >
+            <Button role="submit" loading={loading}>
               Reset password
-            </button>
+            </Button>
           </Form>
         </Formik>
         <div>
           {tokenError ? (
             <div>
-              <div>{tokenError}</div>
-              <NextLink href="/forgot-password">Forgot Password</NextLink>
+              <div className="py-2 text-red-500 text-center">{tokenError}</div>
+              <div className="flex items-center justify-center text-lg text-primary mt-8">
+                <NextLink href="/forgot-password">Forgot Password</NextLink>
+              </div>
             </div>
           ) : null}
         </div>
@@ -69,6 +81,10 @@ ChangePassword.getInitialProps = ({ query }) => {
   return {
     token: query.token as string,
   };
+};
+
+ChangePassword.getLayout = function getLayout(page: ReactElement) {
+  return <AuthScreen>{page}</AuthScreen>;
 };
 
 export default ChangePassword;
